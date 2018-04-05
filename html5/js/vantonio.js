e89a26c514fdfde5cf09aecbcd0ecfe9e726995a
valentin antonio <vale.residencia@gmail.com>
2018-04-05 15:25:55 -0500

    new pdc for magento2

---
+++ html5/js/vantonio.js
@@ -0,0 +1,10 @@
+var canvas = new fabric.Canvas('canvas_vantonio');
+
+        var rect = new fabric.Rect({
+            top : 100,
+            left : 100,
+            width : 60,
+            height : 70,
+            fill : 'red'
+        });
+        canvas.add(rect);
