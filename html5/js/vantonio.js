db9b590a2d6502941a1b18b4b0d7248c382419ff
valentin antonio <vale.residencia@gmail.com>
2018-04-05 15:43:08 -0500

    background image

--- html5/js/vantonio.js
+++ html5/js/vantonio.js
@@ -1,10 +1,25 @@
-var canvas = new fabric.Canvas('canvas_vantonio');
+// var canvas = new fabric.Canvas('canvas_vantonio');
+// var text = new fabric.IText('vantonio', {
+//     left: 100,
+//     top: 100,
+//     fill: 'navy'
+// });
+// canvas.add(text);
 
-        var rect = new fabric.Rect({
-            top : 100,
-            left : 100,
-            width : 60,
-            height : 70,
-            fill : 'red'
-        });
-        canvas.add(rect);
+
+var canvas = new fabric.Canvas('canvas_vantonio');
+//document.getElementById('c1').onclick=function(){
+       fabric.Image.fromURL('https://s5.postimg.org/mcnyutofb/mens_longsleeve_front.png',function(oImg){
+            oImg.scale(0.9).setFlipX(true);//tmñ
+            oImg.left=0;//posicion de imagen
+            oImg.top=0;
+            oImg.selectable=false;
+            canvas.add(oImg);
+        })
+// };
+    canvas.backgroundColor = '#DEB887';
+    canvas.renderAll();
+    $('select#color').on('change', function() {
+      canvas.backgroundColor = this.value;
+        canvas.renderAll();
+    });
